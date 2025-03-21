d77bc2fa8131d656eb22c5c8fc78637746a3131d
ThibSara <sara.thibierge@gmail.com>
2025-03-21 14:18:28 +0100

    Remove StepSequencer component from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import StepSequencer from "@/components/StepSequencer";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
-      <StepSequencer />
     </div>
   );
 }
