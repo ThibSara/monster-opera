7e0763234df4898360b9b7ab4862fadf70b5009b
ThibSara <sara.thibierge@gmail.com>
2025-03-21 14:22:33 +0100

    Add StepSequencer component to Home page for enhanced functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import StepSequencer from "@/components/StepSequencer";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <StepSequencer />
     </div>
   );
 }
